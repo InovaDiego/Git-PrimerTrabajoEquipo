fc374657634611b899e722c1b7dd1e472fcf07c4
Regina-Felix <lgarcia32381@ucq.edu.mx>
2024-06-14 17:48:25 -0600

    Fun Lorena R.

--- Funciones/Funciones/LorenaReginaGarciaFelix.cpp
+++ Funciones/Funciones/LorenaReginaGarciaFelix.cpp
@@ -1,7 +1,8 @@
 #include "LorenaReginaGarciaFelix.h"
 #include <algorithm>
 
-bool esPalindromo(const std::string& palabra) {
+bool esPalindromo(const std::string& palabra) 
+{
     std::string inversa = palabra;
     std::reverse(inversa.begin(), inversa.end());
     return palabra == inversa;
